9ed5c7c39f3084543ff8dccd8f00d00d9ac30c97
Cristiano Ribeiro Braga <cristiano-ubuntu@hotmail.com>
2023-10-12 21:13:54 -0300

    configura_ubuntu.sh

--- configura_ubuntu.sh
+++ configura_ubuntu.sh
@@ -16,21 +16,6 @@ echo "Criando o script 'atualiza' em /.local/bin..."
 sudo echo "sudo apt-get update && apt-get upgrade && apt-get dist-upgrade && apt-get autoremove && apt-get autoclean && apt-get clean" > ~/.local/bin/atualiza
 chmod +x ~/.local/bin/atualiza
 
-# Instalar e configurar o LightDM
-echo "Executando: sudo apt install lightdm -y"
-sudo apt install lightdm -y
-
-echo "Movendo 50-unity-greeter.conf para 50-unity-greeter.conf.old..."
-sudo mv /usr/share/lightdm/lightdm.conf.d/50-unity-greeter.conf /usr/share/lightdm/lightdm.conf.d/50-unity-greeter.conf.old
-
-echo "Editando 50-xfce-greeter.conf..."
-sudo tee /usr/share/lightdm/lightdm.conf.d/50-xfce-greeter.conf <<EOL
-[SeatDefaults]
-greeter-session=unity-greeter
-user-session=xfce
-
-####
-
 # Adiciona o repositório PPA do Mozilla Team
 echo "Adicionando o repositório PPA do Mozilla Team..."
 sudo add-apt-repository ppa:mozillateam/ppa -y
@@ -56,7 +41,19 @@ sudo apt update
 echo "Instalando o Firefox..."
 sudo apt install firefox -y
 
-####
+# Instalar e configurar o LightDM
+echo "Executando: sudo apt install lightdm -y"
+sudo apt install lightdm -y
+
+echo "Movendo 50-unity-greeter.conf para 50-unity-greeter.conf.old..."
+sudo mv /usr/share/lightdm/lightdm.conf.d/50-unity-greeter.conf /usr/share/lightdm/lightdm.conf.d/50-unity-greeter.conf.old
+
+echo "Editando 50-xfce-greeter.conf..."
+sudo tee /usr/share/lightdm/lightdm.conf.d/50-xfce-greeter.conf <<EOL
+[SeatDefaults]
+greeter-session=unity-greeter
+user-session=xfce
+
 # Exibir mensagem de conclusão
 echo "Todas as instalações foram concluídas com sucesso! O sistema será reiniciado em 10 segundos..."
 sleep 10
