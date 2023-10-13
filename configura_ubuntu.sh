0acfb474b4a536542d3c2eaa15355ffe5c5c4017
Cristiano Ribeiro Braga <cristiano-ubuntu@hotmail.com>
2023-10-12 22:28:48 -0300

    configura_ubuntu.sh

--- configura_ubuntu.sh
+++ configura_ubuntu.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
 
 # Atualizar pacotes
-echo "Executando: sudo apt update -y"
 sudo apt update -y
 
 # Instalar pacotes
-echo "Executando: sudo apt install xorg xfce4 xfce4-terminal -y"
 sudo apt install xorg xfce4 xfce4-terminal -y
-
-echo "Executando: sudo apt install python3 python3-pip zip git ffmpeg thunar-archive-plugin -y"
 sudo apt install python3 python3-pip zip git ffmpeg thunar-archive-plugin -y
+
+# Cria a pasta /.local/bin
+mkdir ~/.local/bin
 
 # Criar o script 'atualiza'
 echo "Criando o script 'atualiza' em /.local/bin..."
@@ -17,11 +16,9 @@ sudo echo "sudo apt-get update && apt-get upgrade && apt-get dist-upgrade && apt
 chmod +x ~/.local/bin/atualiza
 
 # Adiciona o repositório PPA do Mozilla Team
-echo "Adicionando o repositório PPA do Mozilla Team..."
 sudo add-apt-repository ppa:mozillateam/ppa -y
 
 # Cria e adiciona as configurações de prioridade do Firefox no PPA
-echo "Configurando a prioridade do Firefox no PPA..."
 echo 'Package: firefox*
 Pin: release o=LP-PPA-mozillateam
 Pin-Priority: 501
@@ -30,19 +27,15 @@ Pin: release o=Ubuntu
 Pin-Priority: -1' | sudo tee /etc/apt/preferences.d/mozillateamppa
 
 # Configura atualizações automáticas
-echo "Configurando atualizações automáticas..."
 echo 'Unattended-Upgrade::Allowed-Origins:: "LP-PPA-mozillateam:${distro_codename}";' | sudo tee /etc/apt/apt.conf.d/51unattended-upgrades-firefox
 
 # Atualiza os repositórios
-echo "Atualizando os repositórios..."
 sudo apt update
 
 # Instala o Firefox
-echo "Instalando o Firefox..."
 sudo apt install firefox -y
 
 # Instalar e configurar o LightDM
-echo "Executando: sudo apt install lightdm -y"
 sudo apt install lightdm -y
 
 echo "Movendo 50-unity-greeter.conf para 50-unity-greeter.conf.old..."
@@ -54,8 +47,6 @@ sudo tee /usr/share/lightdm/lightdm.conf.d/50-xfce-greeter.conf <<EOL
 greeter-session=unity-greeter
 user-session=xfce
 
-# Exibir mensagem de conclusão
-echo "Todas as instalações foram concluídas com sucesso! O sistema será reiniciado em 10 segundos..."
 sleep 10
 
 # Reiniciar o sistema
