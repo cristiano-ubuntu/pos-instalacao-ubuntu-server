57e67e7b45f82124d534eeb942e785e3d9eaa877
Cristiano Ribeiro Braga <cristiano-ubuntu@hotmail.com>
2023-10-14 23:54:11 -0300

    ubuntu_final.sh

--- ubuntu_final.sh
+++ ubuntu_final.sh
@@ -10,13 +10,16 @@ sudo apt install nano mousepad python3 python3-pip xarchiver catfish ffmpeg thun
 # sudo apt install  policykit-1-gnome -y - comentei pq na linha de cima já instala o policykit-1
 
 # Cria a pasta /bin
-mkdir /home/leo/bin
+mkdir -p ~/.local/bin
 
-# Criar o script 'atualiza' em /usr/local/bin/
-echo 'apt-get update && apt-get upgrade && apt-get dist-upgrade && apt-get autoremove && apt-get autoclean && apt-get clean' | sudo tee /usr/bin/atualiza
+# Criar o script 'atualiza' em /.local/bin
+echo 'apt-get update && apt-get upgrade && apt-get dist-upgrade && apt-get autoremove && apt-get autoclean && apt-get clean' | sudo tee ~/.local/bin/atualizar
 
-# Dá permissão de execução ao script atualiza
-sudo chmod +x /usr/bin/atualiza
+# Adicionar a pasta ~/.local/bin ao PATH
+export PATH="$PATH:$HOME/.local/bin"
+
+# Dá permissão de execução ao script atualizar
+sudo chmod +x /.local/bin/atualizar
 
 # Adiciona o repositório PPA do Grub Customizer
 sudo add-apt-repository ppa:danielrichter2007/grub-customizer/ppa -y
